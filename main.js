f3cda8f7a8a6ddbd971c023c9ef0d7d7d593d7e1
Leonardo Mazzuco <leo.mazzuco@gmail.com>
2023-10-16 22:08:58 -0300

    Firt Commit

---
+++ main.js
@@ -0,0 +1,13 @@
+alert("Descubra sua idade em 2035!")
+
+
+var  texto = prompt("Digite seu Nome.")
+var numero = prompt("Digite o ano em que você nasceu.")
+
+var ano = 2035
+
+
+var resultado = parseInt (ano) - parseInt(numero)
+
+
+console.log("Olá, " +texto,"em 2035 você terá " +resultado,"anos.")
